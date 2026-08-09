8d6ab90b4a4b972f82b4ef2299263014b93a0bfb
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2026-08-09 17:37:51 +0200

    test: test config site with full url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vincentfqt.github.io",
+  site: "https://vincentfqt.github.io/astro-portfolio",
   trailingSlash: "never",
 });
