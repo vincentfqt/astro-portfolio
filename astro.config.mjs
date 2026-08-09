a8172ab46616fec75fe04b89388e60aa0c660a57
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2026-08-09 17:33:07 +0200

    test: test config no base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vincentfqt.github.io",
-  base: "/astro-portfolio",
+  trailingSlash: "never",
 });
