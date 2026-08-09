50bd4f0140a0fdc3ee797bc52ea2e6bcbec2eb6a
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2026-08-09 17:48:03 +0200

    chore: undo config url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vincentfqt.github.io/astro-portfolio",
-  trailingSlash: "never",
+  site: "https://vincentfqt.github.io",
+  base: "/astro-portfolio",
 });
