b324646670b1e5923dbf6b1738a7963c7be08814
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2026-08-10 23:46:18 +0200

    chore: add Preact Package and config to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vincentfqt.github.io",
   base: "/astro-portfolio",
+  integrations: [preact()],
 });
