1094eb1e26ea39edf0c88d9274637d08e5517a87
vincentfqt <89091045+vincentfqt@users.noreply.github.com>
2026-08-11 00:42:51 +0200

    refacto: use getCollection function for fetch posts Collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("blog");
+
   return rss({
     title: "Apprenti Astro | Blog",
     description: "Mon parcours d'apprentissage d'Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/astro-portfolio/posts/${post.id}/`,
+    })),
     customData: `<language>fr-fr</language>`,
   });
 }
